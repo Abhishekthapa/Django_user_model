6de7711f3feabb87dadef56f18167591b6821bb1
Abhishek.Thapa <abhishek.thapa@cedargate.com>
2021-08-26 13:49:04 +0545

    Changed engine and master table versions and processing scripts for Engine Release V 11

--- check.py
+++ check.py
@@ -109,5 +109,4 @@ if __name__ == '__main__':
     repository.git.add("./check.py", "./checktest")
     repository.git.commit('-m',
                           'Changed engine and master table versions and processing scripts for Engine Release V ' + engineVersion)
-    origin = repository.remote(name='origin')
-    origin.push()
+    repository.git.push("origin", "usermodelframeworkbranch")
