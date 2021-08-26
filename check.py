196df76c2dcbb1fb6f7bed8e3610f36cd09be9c9
Abhishek.Thapa <abhishek.thapa@cedargate.com>
2021-08-26 13:35:00 +0545

    checking

--- check.py
+++ check.py
@@ -63,50 +63,51 @@ if __name__ == '__main__':
     origin = repository.remote(name='origin')
     origin.pull()
 
-    # change the file input to ../src/main/resources/pa_engine_version.properties
-    change_version_for_pa_engine_properties("checktest", "engineVersion=", engineVersion)
-    # change the file input to ../src/main/resources/pa_engine_version.properties
-    if stageVersion:
-        change_version_for_pa_engine_properties("checktest", "stageVersion=",
-                                                stageVersion)
-
-    # update engine version in pom.xml
-
-    # change file location to "../pom.xml"
-    with fileinput.FileInput("checktest", inplace=True) as file:
-        version = 0
-        for line in file:
-            if "<version>" in line:
-                version = line.split("<version>")[1]
-                print(line.replace(version, engineVersion + "</version>"), end='\n')
-            else:
-                print(line, end="")
-
-        print("version changed to: ", engineVersion, " in pom.xml")
-
-    # change jarversion for bash and python scripts.
-    change_JARversion_for_pyfiles("checktest", "DasCascading-", engineVersion)
-
-    # # update processing scripts
-    # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/jobs/production_cascading.py", "DasCascading-", engineVersion)
-    # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/jobs/emr_production_cascading.py", "DasCascading-", engineVersion)
+    # # change the file input to ../src/main/resources/pa_engine_version.properties
+    # change_version_for_pa_engine_properties("checktest", "engineVersion=", engineVersion)
+    # # change the file input to ../src/main/resources/pa_engine_version.properties
+    # if stageVersion:
+    #     change_version_for_pa_engine_properties("checktest", "stageVersion=",
+    #                                             stageVersion)
     #
-    # # # update parallel processing script
-    # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/jobs/parallel_emr_production_cascading.py", "DasCascading-", engineVersion)
+    # # update engine version in pom.xml
     #
-    # # # update real time migration script
-    # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/jobs/realTime_migration.py", "DasCascading-", engineVersion)
+    # # change file location to "../pom.xml"
+    # with fileinput.FileInput("checktest", inplace=True) as file:
+    #     version = 0
+    #     for line in file:
+    #         if "<version>" in line:
+    #             version = line.split("<version>")[1]
+    #             print(line.replace(version, engineVersion + "</version>"), end='\n')
+    #         else:
+    #             print(line, end="")
     #
-    # # # update masterTables.sh
-    # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/bin/masterTables.sh", "DasCascading-", engineVersion)
+    #     print("version changed to: ", engineVersion, " in pom.xml")
     #
-    # # # update historicalToPx.sh
-    # change_JARversion_for_pyfiles("historicalToPx.sh", "DasCascading-", engineVersion)
+    # # change jarversion for bash and python scripts.
+    # change_JARversion_for_pyfiles("checktest", "DasCascading-", engineVersion)
     #
-    # # # update historicalToPxEmr.sh
-    # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/bin/historicalToPxEmr.sh", "DasCascading-", engineVersion)
-
-    repository.git.add("./check.py", "./checktest")
-    repository.git.commit('-m',
-                          'Changed engine and master table versions and processing scripts for Engine Release V ' + engineVersion)
-    repository.remotes.origin.push()
+    # # # update processing scripts
+    # # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/jobs/production_cascading.py", "DasCascading-", engineVersion)
+    # # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/jobs/emr_production_cascading.py", "DasCascading-", engineVersion)
+    # #
+    # # # # update parallel processing script
+    # # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/jobs/parallel_emr_production_cascading.py", "DasCascading-", engineVersion)
+    # #
+    # # # # update real time migration script
+    # # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/jobs/realTime_migration.py", "DasCascading-", engineVersion)
+    # #
+    # # # # update masterTables.sh
+    # # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/bin/masterTables.sh", "DasCascading-", engineVersion)
+    # #
+    # # # # update historicalToPx.sh
+    # # change_JARversion_for_pyfiles("historicalToPx.sh", "DasCascading-", engineVersion)
+    # #
+    # # # # update historicalToPxEmr.sh
+    # # change_JARversion_for_pyfiles("../src/main/resources/dasProduction/bin/historicalToPxEmr.sh", "DasCascading-", engineVersion)
+    #
+    # repository.git.add("./check.py", "./checktest")
+    # repository.git.commit('-m',
+    #                       'Changed engine and master table versions and processing scripts for Engine Release V ' + engineVersion)
+    # origin = repository.remote(name='origin')
+    # origin.push()
