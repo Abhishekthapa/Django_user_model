600b5d69e1c6d8bd42b618f1dec849807d1c1e9a
Abhishek.Thapa <abhishek.thapa@cedargate.com>
2021-08-26 14:57:22 +0545

    Changed engine and master table versions and processing scripts for Engine Release V 90

--- check.py
+++ check.py
@@ -113,5 +113,6 @@ if __name__ == '__main__':
     repository.git.commit('-m',
                           'Changed engine and master table versions and processing scripts for Engine Release V ' + engineVersion)
 
-    origin.push()
+    g = git.cmd.Git('/home/Abhishek.Thapa/Desktop/SimpleWebsite-with-Django_user_model')
+    g.push()
    # subprocess.call(f'git push -u origin')
